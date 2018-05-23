b7cdca2d9b5246cd86e8b89cd94c14f65dcdd4f4
Varun Iyer <vi.mai@protonmail.ch>
2018-05-23 00:10:20 -0700

    fixed stuff

--- train.py
+++ train.py
@@ -9,14 +9,27 @@ from Vector import Vector
 from Hypothesis import Hypothesis
 from TrainingExample import TrainingExample
 
-ts = TrainingSet(Hypothesis(Vector(randint(-5, 5), randint(-5, 5))), Vector.fill(-1, 2), Vector.fill(1, 2), .1, 20)
+# initial learning stuff
+coeff = Vector(randint(-5, 5), randint(-5, 5))
+ts = TrainingSet(Hypothesis(coeff), Vector.fill(-1, 2), Vector.fill(1, 2), .1, 20)
 
-h = Hypothesis(2)
+# data to plot surface
+surface_0 = np.arange(-5, 5, 0.25)
+surface_1 = np.arange(-5, 5, 0.25)
+err = []
+surface_0, surface_1 = np.meshgrid(surface_0, surface_1)
 
-# y = theta_0 * x + theta_1
+for s0, s1 in zip(surface_0, surface_1):
+    err.append(ts.error(Hypothesis(Vector(s0, s1))))
+
+
+#make h start at the highest point so it's dramatic
+m_idx = np.argmax(err)
+h = Hypothesis(Vector(np.ndarray.flatten(surface_0)[m_idx], np.ndarray.flatten(surface_1)[m_idx]))
+print(h.params)
+
 theta_0 = []
 theta_1 = []
-
 errors = []
 
 for i in range(1000):
@@ -28,24 +41,15 @@ for i in range(1000):
 	theta_1.append(h.params[1])
 	errors.append(ts.error(h))
 
-surface_0 = []
-surface_1 = []
-surface_err = []
+print("Actual values: {}".format(coeff))
+print("Learned values: {}".format(h.params))
+print("Percent error: {}".format(100 * sum([abs(c/p) for c, p in zip(coeff - h.params, h.params)])/len(coeff)))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-# Make data.
-surface_0 = np.arange(-5, 5, 0.25)
-surface_1 = np.arange(-5, 5, 0.25)
-err = []
-surface_0, surface_1 = np.meshgrid(surface_0, surface_1)
-
-for s0, s1 in zip(surface_0, surface_1):
-	err.append(ts.error(Hypothesis(Vector(s0, s1))))
-
 surf = ax.plot_surface(surface_0, surface_1, err, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 ax.plot(theta_0, theta_1, errors)
 plt.show()
+
